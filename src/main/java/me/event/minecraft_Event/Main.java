3df22d8f4198c9f11eab325741a8f50eaccf1cff
Nikita Huliuk <nikaron4ik@gmail.com>
2025-01-08 00:03:01 +0300

    feat: Написан функционал начального меню ивента
    - Теперь предметы нельзя брать/перемещать
    - При нажатии на любую из лошадей она начинает подсвечиваться цветом зачарования (механика выбора лошади)
     (Проблема - на данный момент сохраняется именно значение её слота, по которому потом придётся выбирать лошадь в основном меню, которое делает Артём)
    - При нажатии на барьер меню закрывается.

--- src/main/java/me/event/minecraft_Event/Main.java
+++ src/main/java/me/event/minecraft_Event/Main.java
@@ -1,6 +1,7 @@
 package me.event.minecraft_Event;
 
 import me.event.minecraft_Event.commands.EventCMD;
+import me.event.minecraft_Event.listeners.EventStartMenuListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -8,6 +9,8 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("event_start").setExecutor(new EventCMD(this));
+
+        getServer().getPluginManager().registerEvents(new EventStartMenuListener(), this);
 
     }
 
--- src/main/java/me/event/minecraft_Event/listeners/EventStartMenuListener.java
+++ src/main/java/me/event/minecraft_Event/listeners/EventStartMenuListener.java
@@ -1,4 +1,60 @@
 package me.event.minecraft_Event.listeners;
 
-public class EventStartMenuListener implements Listener{
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.raid.RaidStopEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static me.event.minecraft_Event.utils.Utils.color;
+
+public class EventStartMenuListener implements Listener {
+
+    // Хэш-таблица со значением Игрок-Слот для сохранения результата выбранной лошади
+    private Map<Player, Integer> selectedHorses = new HashMap<>();
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getView().getTitle().equals(color("&2&lКонные скачки"))) {
+            // Здесь сразу отменяется действие (P.s Никита: должно ли это реализовываться таким образом, или сделать так, чтобы предметы нельзя было забрать, можно иначе?")
+            event.setCancelled(true);
+
+            // Слоты всех кнопок для выполнения тех или иных действий
+            List<Integer> horses = Arrays.asList(3, 12, 21, 30);
+            Integer next_button = 14;
+            Integer exit_button = 23;
+
+            int slot = event.getRawSlot();
+            Player player = (Player) event.getWhoClicked();
+
+            // Действия в случае, если выбрана была одна из лошадей
+            if (horses.contains(slot)) {
+                Integer previousSlot = selectedHorses.put(player, slot);
+
+                if (previousSlot != null) {
+                    ItemStack previousItem = event.getInventory().getItem(previousSlot);
+                    if (previousItem != null) {
+                        previousItem.removeEnchantment(Enchantment.INFINITY);
+                    }
+                }
+                ItemStack currentItem = event.getCurrentItem();
+                currentItem.addUnsafeEnchantment(Enchantment.INFINITY, 1);
+
+                player.updateInventory();
+            } else if (slot == next_button) {
+              // Пока что пусто, функционал будет позже
+            } else if (slot == exit_button) {
+                player.closeInventory();
+            }
+
+        }
+    }
 }
+
